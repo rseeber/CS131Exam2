1444cf481243796e732623e3cbd9dbf2a37cfedb
River Seeber <riverseeber12@gmail.com>
2024-10-16 20:02:55 -0700

    init

---
+++ exam2/Main.java
@@ -0,0 +1,5 @@
+package exam2;
+
+public class Main {
+    //this is the class we will use to actually *use* the tree data structure    
+}
---
+++ exam2/Tree.java
@@ -0,0 +1,8 @@
+package exam2;
+
+public class Tree{
+    public static void main(String[] args){
+        System.out.println("Hello, World\nTODO: implement tree data structure");
+    }
+
+}
