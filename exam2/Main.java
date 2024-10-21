edefffa262c46aa58ee12197e5a563a74b69f006
River Seeber <rseeber@proton.me>
2024-10-21 11:17:44 -0700

    add reverse func, general cleanup

--- exam2/Main.java
+++ exam2/Main.java
@@ -3,7 +3,18 @@ package exam2;
 public class Main {
    
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Tree t = new Tree();
+        //insert all the following data points
+        int data[] = {4, 2, 7, 1, 3, 6, 9};
+        int dataLen = 7;
+        //iterate through each index of the array
+        for(int i = 0; i < dataLen; ++i){
+            t.insert(data[i]);
+        }
+        //print the tree before and after reversing it
+        t.printTree();
+        t.reverse();
+        t.printTree();
     }
    
 }
--- exam2/Tree.java
+++ exam2/Tree.java
@@ -9,27 +9,32 @@ public class Tree{
     Tree parent;
     //each node has a key
     int key;
-
+    //creates a vacant tree
     public Tree(){
         this.vacant = true;
         this.parent = null;
         this.leftNode = null;
         this.rightNode = null;
     }
+    public Tree(Tree parent){
+        this();
+        this.parent = parent;
+    }
 
+    //initializes a node with a value, and creates 2 vacant (empty) nodes as left and right children
     public void init(int key){
         this.vacant = false;
         this.key = key;
         //initialize any non-vacant nodes with 2 vacant child nodes, which makes insert() implimentation easier
-        this.leftNode = new Tree();
-        this.rightNode = new Tree();
+        this.leftNode = new Tree(this);
+        this.rightNode = new Tree(this);
     }
-
+    //overload, which assigns a parent node
     public void init(Tree parent, int key){
         init(key);
         this.parent = parent;
     }
-
+    //overload, which assigns parent and child values, this time NOT setting the children to vacant
     public void init(Tree parent, Tree leftNode, Tree rightNode, int key){
         init(parent, key);
         this.leftNode = leftNode;
@@ -37,19 +42,43 @@ public class Tree{
     }
 
     //inserts a key into the tree, placing it in one of the vacant leaf nodes
-    public void insert(int key, Tree target, Tree parent){
+    public void insert(int key){
+        Tree t = this;
         //climb down until we find a vacant slot that can hold our key
-        while(!target.vacant){
+        while(!t.vacant){
             //move left if key is smaller than parent
-            if(key < parent.key){
-                target = parent.leftNode;
+            if(key < t.key){
+                t = t.leftNode;
             }
             //move right if key is bigger (or equal to) parent
             else{
-                target = parent.rightNode;
+                t = t.rightNode;
             }
+            t.parent = t;
         }
         //when we find a vacant node, initialize it with the key
-        target.init(parent, key);
+        t.init(key);
+    }
+
+    //reverses the left-right ordering of the entire tree
+    public void reverse(){
+        //base case
+        if(this.vacant){
+            return;
+        }
+        //recursive case
+        //swap left and right nodes
+        Tree buf;
+        buf = this.leftNode;
+        this.leftNode = this.rightNode;
+        this.rightNode = buf;
+        //now swap all the children of the left and right nodes
+        this.leftNode.reverse();
+        this.rightNode.reverse();
+    }
+
+    //prints the tree
+    public void printTree(){
+        //prints the tree
     }
 }
